2d1551a3f2efd4e9161f4b851d0db4f0b60f3d02
Brandon Weaver <a7xncob@gmail.com>
2020-01-06 03:52:13 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  
+  def self.current_user(session_hash)
+    return User.find(session_hash[:user_id])
+  end
+  
+  def self.is_logged_in?(session_hash)
+    return !!session_hash[:user_id]
+  end
+  
 end
